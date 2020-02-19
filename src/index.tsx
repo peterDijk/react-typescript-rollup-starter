704d3afaa237fb9a30d568408cc8547e098eb61f
Sara Bagheri <sara.bagheri@bynder.com>
2020-02-19 15:17:57 +0100

    test

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,4 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./App";
 
-const rootEl = document.getElementById("root");
-
-ReactDOM.render(<App />, rootEl);
+ReactDOM.render(<App />, document.getElementById("root");
