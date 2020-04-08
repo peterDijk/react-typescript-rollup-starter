192db8d0837fff1431a4aa8755c64c506c04d913
Peter van Dijk <peter.van.dijk@bynder.com>
2020-04-08 22:22:31 +0200

    tailwindok

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-// import './style.css';
 import './style/style.css';
+import './style/tailwind.css';
 import { App } from "./App";
 
 const rootEl = document.getElementById("root");
